7da5c7e29d34d37dd81f1c4eec659504788451db
DmitryLukyanov <dmitry.lukyanov@mongodb.com>
2021-03-04 20:41:05 +0300

    Check behavior with ARM and 2.12.0 release.

---
+++ ConsoleAppTest_ForARM/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.IO;
+
+namespace ConsoleAppTest_ForARM
+{
+    class Program
+    {
+        // script to publish: 
+        // dotnet publish -r linux-arm64 -f netcoreapp2.2 --configuration Release
+        static void Main(string[] args)
+        {
+            // Check MongoDB.Libmongocrypt
+
+            try
+            {
+                var libraryVersion = MongoDB.Libmongocrypt.Library.Version;
+                Console.WriteLine(libraryVersion); // should not be reached with ARM
+            }
+            catch (FileNotFoundException e)
+            {
+                // expected path for ARM
+                Console.WriteLine("Expected failing: " + e);
+            }
+        }
+    }
+}
